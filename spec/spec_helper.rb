f648bfc4a61b938d2d64a3d4b978566fe066f48d
vitaminus <partadw@gmail.com>
2016-11-08 11:19:42 +0200

    Add exclude options to config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,6 +37,11 @@ RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
+  config.filter_run_excluding broken: true
+  config.filter_run_excluding deprecated: true
+  config.filter_run_excluding integration: true
+  config.filter_run_excluding preprod: false if ENV["URL"] == 'preprod'
+
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
